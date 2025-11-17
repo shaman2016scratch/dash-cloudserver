f0a19dd001a860089dc8bbc0dd0ae1dc626ce5e3
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-17 21:39:57 +0300

    Update banned_black_ip.browser.js

--- banned_black_ip.browser.js
+++ banned_black_ip.browser.js
@@ -1,6 +1,15 @@
-Black_i_list = fetch('https://shaman2016scratch.github.io/dash-cloudserver/public/blac', {
-                  method: "POST",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-               })
+Black_i_list = fetch('https://shaman2016scratch.github.io/dash-cloudserver/public/ip-blacklist.json', {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+ip = fetch('https://api.ipify.org/', {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+if (Black_i_list.list[ip]) {
+  document = '<p>You are blocked by the cloud server.</p>'
+}
