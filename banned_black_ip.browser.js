dc589fc1e6a3e1c86b2024363d9459d8a66a5ed2
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-17 21:33:35 +0300

    Create banned_black_ip.browser.js

---
+++ banned_black_ip.browser.js
@@ -0,0 +1,6 @@
+Black_i_list = fetch('https://shaman2016scratch.github.io/dash-cloudserver/public/blac', {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+               })
