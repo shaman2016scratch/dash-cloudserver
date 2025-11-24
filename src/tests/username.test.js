85cf339823127e4236afb58de968e6c71ab29dcd
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-24 20:22:03 +0300

    Update username.test.js

--- src/tests/username.test.js
+++ src/tests/username.test.js
@@ -1,12 +1,12 @@
 const username = require('../username');
 
 test('isGenerated', () => {
-  expect(username.isGenerated('player')).toBe(false);
-  expect(username.isGenerated('player123')).toBe(true);
-  expect(username.isGenerated('player123456')).toBe(true);
-  expect(username.isGenerated('Player123456')).toBe(true);
-  expect(username.isGenerated('player1234563209484')).toBe(false);
-  expect(username.isGenerated('player123e')).toBe(false);
-  expect(username.isGenerated('player_123')).toBe(false);
-  expect(username.isGenerated('eplayer123')).toBe(false);
+  expect(username.isGenerated('user')).toBe(false);
+  expect(username.isGenerated('user123')).toBe(true);
+  expect(username.isGenerated('user123456')).toBe(true);
+  expect(username.isGenerated('User123456')).toBe(true);
+  expect(username.isGenerated('user1234563209484')).toBe(false);
+  expect(username.isGenerated('user123e')).toBe(false);
+  expect(username.isGenerated('user_123')).toBe(false);
+  expect(username.isGenerated('euser123')).toBe(false);
 });
